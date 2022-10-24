d06e5e72e447948355e5e372095cf586fde4dc16
Pavel Karman <karmanpavel.pavlkan@gmail.com>
2022-10-25 00:41:31 +0300

    feat: add sound to moves and mistake moves

--- src/main.js
+++ src/main.js
@@ -4,21 +4,25 @@ import { GemPuzzle } from "./model/GemPuzzle.js";
 import { FrameComponent } from "./view/FrameComponent.js";
 import { GameStateComponent } from "./view/GameStateComponent.js";
 import { MenuComponent } from "./view/MenuComponent.js";
+import { SoundComponent } from "./view/SoundComponent.js";
 
 const root = document.getElementById("root");
 const gemPuzzle = new GemPuzzle(3);
+const soundComponent = new SoundComponent();
 const frameComponent = new FrameComponent();
 const gameStateComponent = new GameStateComponent();
 const frameController = new FrameController(
     gemPuzzle,
     frameComponent,
-    gameStateComponent
+    gameStateComponent,
+    soundComponent
 );
 frameComponent.setController(frameController);
 const menuController = new MenuController(
     gemPuzzle,
     frameComponent,
-    gameStateComponent
+    gameStateComponent,
+    frameController
 );
 const menuComponent = new MenuComponent(menuController);
 root.append(
